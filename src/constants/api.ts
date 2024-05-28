a876ed3e9fb0743933cc7c33a4317f6fdc46c45e
Huy Nguyen <xuanhuybn2403@gmail.com>
2024-05-28 23:00:31 +0700

    update 5

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,9 @@
-export const BASE_URL = process.env.REACT_APP;
+export const BASE_URL = process.env.REACT_APP
 export const Menu = {
-    MENU: `https://divineshop.vn/api/header/menu`
-  };
+  MENU: `https://divineshop.vn/api/header/menu`
+};
+export const Header = {
+  TOP: `https://divineshop.vn/page-data/sq/d/2215486189.json`,
+  MID: `https://divineshop.vn/page-data/sq/d/2247331527.json`,
+  BOT: `https://divineshop.vn/page-data/sq/d/401991559.json`,
+};
---
+++ src/hooks/header/topHeader.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { Header } from "constants/api";
+import httpMethod from "services/httpMethod";
+import { showError } from "helpers/toast";
+
+const useTopheader = (props: any) => {
+    const [data, setData] = useState<any[]>([]);
+    const getTopheader = async () => {
+        try {
+            const response = await httpMethod.get(
+                `${Header.TOP}`,
+            );
+            if (response.status === 200) {
+                setData(response.data.data.homeTopMenu.contact);
+            }
+        } catch (error: any) {
+            showError('Api đâng chết');
+        }
+    };
+    const refresh = () => {
+        getTopheader();
+    };
+    //! render
+    return { refresh, data };
+};
+
+export default useTopheader;
