c7cd63f51ec70f7b710fe2d9c7a1e8d17ebca774
xuan huy <xuanhuybn2403@gmail.com>
2024-10-24 17:30:06 +0700

    update24/10

--- src/hooks/featured/featured.ts
+++ src/hooks/featured/featured.ts
@@ -2,16 +2,11 @@ import { useContext, useEffect, useState } from "react";
 import { Body } from "constants/api";
 import httpMethod from "services/httpMethod";
 import { showError } from "helpers/toast";
-import { AppContext } from "App";
-
 const useFeatured = () => {
   //! state
   const [data, setData] = useState<any[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
-  const { setLoading } = useContext(AppContext);
-
   const APIs = [`${Body.FEATURED}`, `${Body.FEATURED1}`, `${Body.FEATURED2}`];
-
   const getFeatured = async (url: string) => {
     // if (!data) {
     try {
